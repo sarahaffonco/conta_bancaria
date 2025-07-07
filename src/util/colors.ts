3d91d8374ef0b5cc27632ec44523022eff2dc1a5
Sarah Affonco <sarah.affonco@gmail.com.br>
2025-07-07 13:23:54 -0300

    resolucao exercicio

---
+++ src/util/colors.ts
@@ -0,0 +1,10 @@
+import read = require("readline-sync");
+
+export const colors = {
+  reset:  "\x1b[0m",
+  yellow: "\x1b[33m",
+  cyan: "\x1b[36m",
+  magenta: "\x1b[35m",
+  green: "\x1b[32m",
+  red: "\x1b[31m",
+};
